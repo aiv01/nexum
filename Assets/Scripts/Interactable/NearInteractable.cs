d75a90865552b5c34da621d97d4314545fa4bda3
Valvola <biffidavide88@gmail.com>
2023-03-15 17:57:20 +0100

    Added Pressure Plate

--- Assets/Scripts/Interactable/NearInteractable.cs
+++ Assets/Scripts/Interactable/NearInteractable.cs
@@ -10,12 +10,36 @@ public class NearInteractable : MonoBehaviour
     [SerializeField]
     UnityEvent interacted;
 
+    [SerializeField]
+    UnityEvent reset;
 
+    [SerializeField]
+    [Tooltip("Impostare il valore a negativo per evitare il reset")]
+    float resetTime;
+
+    private float currResetTime  = 0f;
     public void Activate()
     {
         Debug.Log("Interacted");
         GetComponent<Collider>().enabled = false;
         interacted.Invoke();
+
+        if (resetTime > 0)
+            currResetTime = resetTime;
+
+    }
+    private void Update()
+    {
+        if (resetTime <= 0) return;
+        if (currResetTime <= 0) return;
+
+        currResetTime -= Time.deltaTime;
+        if (currResetTime <= 0)
+        {
+            Debug.Log("Reset");
+            reset.Invoke();
+            GetComponent<Collider>().enabled = true;
+        }
     }
 
 
---
+++ Assets/Scripts/Interactable/PressurePlate.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    bool requireHeavyObject;
+
+    [SerializeField]
+    Transform plate;
+
+    [SerializeField]
+    float neutralPlateDepth;
+    [SerializeField]
+    float pressedPlateDepth;
+
+    [SerializeField]
+    UnityEvent onPressed;
+
+    [SerializeField]
+    UnityEvent onReleased;
+
+    int _entityPressing;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Premuto");
+        if (requireHeavyObject && other.GetComponent<IsHeavyObject>() == null) return;
+
+        _entityPressing++;
+        if (_entityPressing > 1) return;
+
+        onPressed.Invoke();
+        plate.localPosition = transform.up * pressedPlateDepth;
+        Debug.Log("pressed by " + other.name);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Rilascitao");
+        if (requireHeavyObject && other.GetComponent<IsHeavyObject>() == null) return;
+
+        _entityPressing--;
+        if (_entityPressing > 0) return;
+
+        onReleased.Invoke();
+        plate.localPosition = transform.up * neutralPlateDepth;
+        Debug.Log("releaded by " + other.name);
+
+    }
+
+}
